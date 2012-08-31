f5c59a018a795954e5715d2f81085e4716358a24
Anton Lenev <m0rg0t.Anton@gmail.com>
2012-08-31 23:38:33 +0400

    New images style

---
+++ Noooooo/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Noooooo.Resources;
+using Coding4Fun.Phone.Controls;
+using System.Device.Location;
+using System.Windows.Media.Imaging;
+
+namespace Noooooo
+{    
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MediaElement mediaPlayer = sender as MediaElement;
+            //button1.Content = 
+            this.no.Source = new Uri("/Nooo.mp3", UriKind.Relative);
+            this.no.Volume = 100;
+            this.no.Position = new TimeSpan(0, 0, 0, 0, 1);
+            this.no.Play();
+            //MediaElement 
+        }
+
+
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+        private void ApplicationBarMenuItem_Click(object sender, EventArgs e) //author
+        {
+            AboutPrompt p = new AboutPrompt();
+            p.Show("Anton Lenev (m0rg0t)", "@m0rg0t", "m0rg0t.Anton@gmail.com", @"http://m0rg0t.ru");
+        }
+
+        private void ApplicationBarMenuItem_Click_1(object sender, EventArgs e) //idea
+        {
+            AboutPrompt p = new AboutPrompt();
+            AboutPersonItem item = new AboutPersonItem();
+            item.WebSiteUrl = @"nooooooooooooooo.com";
+            p.Show(item);
+        }
+
+        private void PhoneApplicationPage_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ApplicationBarMenuItem_Click_2(object sender, EventArgs e)
+        {
+            MessageBox.Show("Quote from site: This is not associated with Darth Vader, Star Wars etc... Just my part to add to the hilarity of the world. You're welcome");
+        }
+
+        private void button2_Tap(object sender, GestureEventArgs e)
+        {
+            try
+            {
+                MediaElement mediaPlayer = sender as MediaElement;
+                this.no.Source = new Uri("/Nooo.mp3", UriKind.Relative);
+                this.no.Volume = 100;
+                this.no.Position = new TimeSpan(0, 0, 0, 0, 1);
+                this.no.Play();
+            }
+            catch { };
+        }
+
+        private void button2_ManipulationStarted(object sender, ManipulationStartedEventArgs e)
+        {
+            this.button2.Source = new BitmapImage(new Uri("button_active.png", UriKind.Relative));
+        }
+
+        private void button2_ManipulationCompleted(object sender, ManipulationCompletedEventArgs e)
+        {
+            this.button2.Source = new BitmapImage(new Uri("button.png", UriKind.Relative));
+        }
+    }
+}
